0eca7912bab5311bf67bab0a604883aa2c9d4a3d
Abrar Haq <abrar_alhaq@yahoo.co.in>
2011-06-13 01:46:02 +0530

    Next commit

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -3,10 +3,20 @@ class BookController < ApplicationController
       @books = Book.find(:all)
    end
    def show
+      @book = Book.find(params[:id])
    end
    def new
+      @book = Book.new
+      @subjects = Subject.find(:all)
    end
    def create
+     @book = Book.new(params[:book])
+      if @book.save
+            redirect_to :action => 'list'
+      else
+            @subjects = Subject.find(:all)
+            render :action => 'new'
+      end
    end
    def edit
    end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,4 @@
 class Subject < ActiveRecord::Base
-  
+  has_many :books
+  validates_presence_of :name
 end
