2543e7e57eaa07f6ec7402d8e8415b25778bf9a9
Abrar Haq <abrar_alhaq@yahoo.co.in>
2011-06-13 01:45:14 +0530

    Next commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
-class Book < ActiveRecord::
-
-belongs_to: subject
+class Book < ActiveRecord::Base
+   belongs_to :subject
+  validates_presence_of :title
+  validates_numericality_of :price, :message=>"should be numaric."
 end
